4ddb0a4194bf7664d7bb2c2489cd9b02a13aa7d2
Melissa Morin <melissa.morinmangaillou@gmail.com>
2019-10-16 15:42:19 -0400

    fin

--- src/App.js
+++ src/App.js
@@ -17,18 +17,22 @@ export default class App {
 		etoiles.setAttribute("src", "/images/etoiles.svg");
 		
 		var etoileun = document.createElement("img");
+		this.etoileun = etoileun;
 		etoileun.classList.add("etoileun");
 		etoileun.setAttribute("src", "/images/etoile1.svg");
 		
 		var etoiledeux = document.createElement("img");
+		this.etoiledeux = etoiledeux;
 		etoiledeux.classList.add("etoiledeux");
 		etoiledeux.setAttribute("src", "/images/etoile2.svg");
 
 		var nuage = document.createElement("img");
+		this.nuage = nuage;
 		nuage.classList.add("etoiledeux");
 		nuage.setAttribute("src", "/images/nuage.svg");
 
 		var texte = document.createElement("img");
+		this.texte = texte;
 		texte.setAttribute("src", "/images/texte.svg");
 		texte.style.left = "15px";
 		texte.style.top = "85px";
@@ -53,68 +57,94 @@ export default class App {
 		nuage.style.left = "4px";
 		nuage.style.top = "65px";
 		
-		
+		this.segmentun();
 		/** Ajuster les dimensions ici */
 		// document.body.style.setProperty("--largeur", "250");
 		// document.body.style.setProperty("--hauteur", "250");
 		// this.app = document.getElementById("app");
 		// // this.elementAnime = document.getElementById("elementAnime");
 		// // Premier segment d'animation ici :
-
-		setTimeout(() => {
-			etoileun.classList.add("filante");
-		}, 1000);
 		
-		setTimeout(() => {
-			nuage.classList.add("texte");
-		}, 1000);
-		setTimeout(() => {
-			etoiledeux.classList.add("filantedeux");
-		}, 2000);
+		texte.addEventListener("animationend", e => {
+			this.redemarrer();
+		} )
 		
-		etoileun.addEventListener("animationend", e => {
-			etoileun.style.left = "60px";
-			etoileun.style.zIndex = "1";
-			etoileun.style.top = "33px";
-
-			etoiledeux.style.left = "75px";
-			etoiledeux.style.zIndex = "1";
-			etoiledeux.style.top = "44px";
-
-			texte.style.opacity = "1";
-			nuage.style.opacity = "1";
-
-			texte.classList.add("texte");
-
-			
+		window.addEventListener("dblclick", e => {
+			this.redemarrer();
 		})
-		
-		etoiledeux.addEventListener("animationend", e => {
-			texte.classList.add("bouger");
-		})
-		
 		// this.rougit();
 		// // Boucle de controle
 		// this.app.addEventListener("animationend", e => {
-		// 	e.stopImmediatePropagation();
-		// 	console.log("fin d'une animation.");
-		// 	// À la fin d'un segment, on démarre l'autre segment. 
-		// 	// Voir l'exemple pour la procédure
-		// });
-	}
-	/**
-	 * On redémarre l'animation
-	 */
-
-	 static segmentun() {
+			// 	e.stopImmediatePropagation();
+			// 	console.log("fin d'une animation.");
+			// 	// À la fin d'un segment, on démarre l'autre segment. 
+			// 	// Voir l'exemple pour la procédure
+			// });
+		}
+		/**
+		 * On redémarre l'animation
+		 */
+		
+		static segmentun() {
+			setTimeout(() => {
+				this.etoileun.classList.add("filante");
+			}, 1000);
+			
+			setTimeout(() => {
+				this.nuage.classList.add("texte");
+			}, 1000);
+			setTimeout(() => {
+				this.etoiledeux.classList.add("filantedeux");
+			}, 2000);
+			
+			this.etoileun.addEventListener("animationend", e => {
+				this.etoileun.style.left = "60px";
+				this.etoileun.style.zIndex = "1";
+				this.etoileun.style.top = "33px";
+	
+				this.etoiledeux.style.left = "75px";
+				this.etoiledeux.style.zIndex = "1";
+				this.etoiledeux.style.top = "44px";
+	
+				this.texte.style.opacity = "1";
+				this.nuage.style.opacity = "1";
+	
+				this.texte.classList.add("texte");
+	
+				
+			})
+			
+			this.etoiledeux.addEventListener("animationend", e => {
+				this.texte.classList.add("bouger");
+			})
 		 
 	 }
 
 	 static redemarrer() {
-		 etoileun.removeAttribut("class");
-		 etoiledeux.removeAttribut("class");
-		 nuage.removeAttribut("class");
-		 texte.removeAttribut("class");
+		this.etoileun.classList.remove("filante");
+		this.etoiledeux.classList.remove("filantedeux");
+		this.nuage.classList.remove("texte");
+		this.texte.classList.remove("texte");
+		this.texte.classList.remove("bouger");
+
+		this.etoileun.style.left = "-100px";
+		this.etoileun.style.top = "33px";
+
+		this.etoiledeux.style.left = "-135px";
+		this.etoiledeux.style.top = "44px";
+
+		this.texte.style.opacity = "0.0";
+		this.texte.style.width = "75%";
+		this.texte.style.left = "58px";
+		this.texte.style.top = "97px";
+
+		this.nuage.style.opacity = "0.0";
+		this.nuage.style.left = "4px";
+		this.nuage.style.top = "65px";
+
+		this.segmentun();
+
+		 return;
 	 }
 	// Créer la fonction statique redemarrer
 	// Enlever toute classe pouvant avoir été ajoutée par un segment d'animation. On peut faire ...removeAttribut("class").
